92127ad19811668d94f59e97b376be790362f40f
saladrai <saladrai@gmail.com>
2016-11-11 12:04:11 +0200

    EEP-7 done

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from base.models import User
+from base.models import User, Institution, Building
 
 
 admin.site.register(User)
+admin.site.register(Institution)
+admin.site.register(Building)
--- base/models.py
+++ base/models.py
@@ -14,6 +14,10 @@ class User(AbstractUser, MPTTModel):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    class Meta:
+        verbose_name = 'Користувач'
+        verbose_name_plural = 'Користувачі'
+
     def __str__(self):
         return "%s %s. %s." % (self.last_name, self.first_name[0], self.middle_name[0])
 
@@ -22,3 +26,28 @@ class User(AbstractUser, MPTTModel):
 
     def get_short_name(self):
         return self.first_name
+
+
+class Institution(models.Model):
+    name = models.CharField('Назва закладу', max_length=500)
+
+    class Meta:
+        verbose_name = 'Заклад, установа'
+        verbose_name_plural = 'Заклади, установи'
+
+    def __str__(self):
+        return self.name
+
+
+class Building(MPTTModel):
+    name = models.CharField('Будівля', max_length=500)
+    institution = models.ForeignKey(Institution, verbose_name='Заклад')
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True,
+                            verbose_name='Орендодавець')
+
+    class Meta:
+        verbose_name = 'Будівля'
+        verbose_name_plural = 'Будівлі'
+
+    def __str__(self):
+        return "%s %s" % (self.name, self.institution)
