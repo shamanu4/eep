df0a9bcb6dc3e7fd56cbe901e4bada76731a9b25
Maxim Musayev <shamanu4@gmail.com>
2016-11-21 12:14:02 +0200

    pipelines an tox setup

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomepageTestCase(TestCase):
+    def test_login_page(self):
+        """Visiting the application homepage should display a nice welcome message"""
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Програма Енергоеффективності міста Умань")
